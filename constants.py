89d1d0133bf0d0e154374b3c648d6df9aeb60002
Scott Macdonald <scott.scotm@gmail.com>
2014-12-08 11:02:10 +0000

    Decoupling dependency on main project

---
+++ constants.py
@@ -0,0 +1,5 @@
+__author__ = 'scotm'
+
+postcodeareas = set(
+    ["AB", "DD", "DG", "EH", "FK", "HS", "IV", "KA", "KW", "KY", "ML", "PA", "PH", "TD", "ZE"] + ["G%d" % i for i in
+                                                                                                  range(1, 10)])
