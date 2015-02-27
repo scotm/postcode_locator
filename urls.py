e516eb61b5cc524b33df423874d1fdf58bb8cc79
Scott Macdonald <scott.scotm@gmail.com>
2015-02-27 12:44:29 +0000

    Making the url a named route - for reuse elsewhere

--- urls.py
+++ urls.py
@@ -5,5 +5,5 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('',
-                       url(r'^map$', PostcodeMappingView.as_view()),
+                       url(r'^map$', PostcodeMappingView.as_view(), name='postcode_point'),
 )
