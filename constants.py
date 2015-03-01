760a2be5a4ce01d443ad859dd7f3eecc61217283
Scott Macdonald <scott.scotm@gmail.com>
2015-03-01 23:03:13 +0000

    Add a postcode prefix regex to the locator module - may come in handy when importing postcodes from CSV files

--- constants.py
+++ constants.py
@@ -1,5 +1,8 @@
 __author__ = 'scotm'
+import re
 
 postcodeareas = set(
     ["AB", "DD", "DG", "EH", "FK", "HS", "IV", "KA", "KW", "KY", "ML", "PA", "PH", "TD", "ZE"] + ["G%d" % i for i in
                                                                                                   range(1, 10)])
+
+postcodeprefix_re = re.compile("^"+"|".join(postcodeareas))
